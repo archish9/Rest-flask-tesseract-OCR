11e7ae1db05bfa99e2da3fb593c30b6b7c452707
archish <11171599+sanatani9@users.noreply.github.com>
2023-02-13 10:59:28 -0600

    more code

--- resources/ocr_core.py
+++ resources/ocr_core.py
@@ -11,36 +11,54 @@ import re
 
 
 bpl = Blueprint("getss",__name__,description="OCR REST API")
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bpl.route('/image')
 class restApi(MethodView):
+
+	
 
 	def __init__(self):
 		if not request.files:
 			abort(400)
 
+	
+
 	#@bpl.response(201,Schema)
 	def post(self):
 
 		file = request.files['file']
-		filename = ''
+		filecheckname = file.filename
+		savefilename = ''
 
-		filename = str(uuid.uuid4()) + '.' + request.files['file'].filename.split('.')[1]
+		if file and allowed_file(file.filename):
 
-		#filename = secure_filename(file.filename)
+			savefilename = str(uuid.uuid4()) + '.' + request.files['file'].filename.split('.')[1]
 
-		# filepath to save image
-		file.save(os.path.join(os.getcwd(), 'static', 'upload', 'image', '', filename))
+			#filename = secure_filename(file.filename)
 
-		custom_config = r'-l eng --oem 3 --psm 1'
-		_text_ = pytesseract.image_to_string(
-		    Image.open(os.path.join(os.getcwd(), 'static', 'upload', 'image', '', filename)),
-		    output_type=Output.DICT,
-		    config=custom_config
-		    )
+			# filepath to save image
+			file.save(os.path.join(os.getcwd(), 'static', 'upload', 'image', '', savefilename))
 
-		image_text = list(dict.fromkeys(re.sub("[^A-Z 0-9]", "", _text_['text'],0,re.IGNORECASE).split(" ")))
-		#print(image_text)
+			custom_config = r'-l eng --oem 3 --psm 1'
+			_text_ = pytesseract.image_to_string(
+			    Image.open(os.path.join(os.getcwd(), 'static', 'upload', 'image', '', savefilename)),
+			    output_type=Output.DICT,
+			    config=custom_config
+			    )
 
-		# response = 'File successfully uploaded'
-		return jsonify({"text": image_text}), 201
+			image_text = list(dict.fromkeys(re.sub("[^A-Z 0-9]", "", _text_['text'],0,re.IGNORECASE).split(" ")))
+			#print(image_text)
+
+			os.remove(os.path.join(os.getcwd(), 'static', 'upload', 'image', '', savefilename))
+
+			# response = 'File successfully uploaded'
+			if len(image_text) > 1:
+				return jsonify({"text": image_text}), 200
+			else:
+				return jsonify({"text": 'No text found in image'}), 400
+		else:
+			return jsonify({"message": 'Allowed file types are png, jpg, jpeg'}), 400
